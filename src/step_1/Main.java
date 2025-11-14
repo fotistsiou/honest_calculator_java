d4e25eba661a1a62c4c084e45aa3bd3d100121f2
fotistsiou <fotis.tsioumas@outlook.com>
2025-11-14 09:21:47 +0200

    Step 1

--- src/step_1/Main.java
+++ src/step_1/Main.java
@@ -1,4 +1,23 @@
 package step_1;
 
+/**
+ * Data collection
+ * ---------------
+ * Description
+ * We will start by implementing the flowchart below. Copy the messages carefully and assign them to the program
+ * variables. It makes no difference whether you make a list or each message is a separate variable. The appropriate
+ * messages must be displayed according to the flowchart.
+ * [image_step_1]
+ * ---------------
+ * Objectives
+ * Implement the flowchart above. Please, follow our recommendations:
+ * The variable calc should have the following format: x operation y. For example: 2 + 3, 2 + g or 3.1 r 5;
+ * The variables x and y must be of the float or int type. The oper variable is a one-character string. Check whether the passed values have proper types. The delimiter must be a dot;
+ * Copy the messages below carefully. The tests will check if the correct message appears in the correct order. Please, do not add extra lines or characters.
+ * msg_0 = "Enter an equation"
+ * msg_1 = "Do you even know what numbers are? Stay focused!"
+ * msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+ */
+
 public class Main {
 }
